45f9f1b1049659fc4c1b8d61c6ff02d56cdb824f
mahabbat <zakariyayevmahabbat@gmail.com>
2022-07-19 00:06:39 +0400

    nine

--- app/views.py
+++ app/views.py
@@ -1,25 +1,41 @@
 from django.shortcuts import redirect, render,HttpResponse
 from django.contrib import messages
+from matplotlib.style import context
 # Create your views here.
 from .forms import  Articleform
+from .models import Article
+
+
 def index(request):
-    
     return  render(request,"index.html")
+
+
+
 def about(request,id):
     context = {'id':id}
     return HttpResponse("about")
+
+
+
+
+
 def dashboard(request):
-    return render(request,"dashboard.html",)
+    articles = Article.objects.filter(author=request.user)
+    context={'articles': articles}
+
+    return render(request,"dashboard.html",context)
+
+
 
 def addArticle(request):
     form = Articleform(request.POST or None,request.FILES or None)
 
     if form.is_valid():
         article = form.save(commit=False)
-        
         article.author = request.user
         article.save()
-
         messages.success(request,"Makale başarıyla oluşturuldu")
         return redirect("index")
+
+        
     return render(request,"addarticle.html",{"form":form})
